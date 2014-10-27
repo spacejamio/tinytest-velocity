e46699501aaf468b6a73ea0d646d2f778b53fd51
Ronen Babayoff <ronen@lavaina.com>
2014-10-27 02:06:54 -0400

    Update loglevel version

--- package.js
+++ package.js
@@ -19,7 +19,7 @@ Package.onUse(function (api) {
 
   api.use(['velocity:core', 'velocity:shim']);
 
-  api.use(['spacejamio:loglevel', 'spacejamio:chai']);
+  api.use(['spacejamio:loglevel@1.1.0_2', 'spacejamio:chai']);
 
   api.imply('velocity:html-reporter', 'client');
 
